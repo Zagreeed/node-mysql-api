0eb8de3adf6047a6aa5e22bc769ed3b64cbefb80
Danley Yap, Galan <galandanley701254@gmail.com>
2026-04-19 16:12:41 +0800

    attemp 2 day 3 (final commit ADDED HOT FIXES)

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -12,15 +12,30 @@ export default function authorize(roles: any = []) {
     return [
         expressjwt({ secret, algorithms: ['HS256'] }),
         async (req: any, res: any, next: any) => {
-            const account = await db.Account.findByPk(req.user.id);
+            const account = await db.Account.findByPk(req.auth.id); // <-- AFTER
+
+            // if (!account || (roles.length && !roles.includes(account.role))) {
+            //     return res.status(401).json({ message: 'Unauthorized' });
+            // }
+
+            // req.auth.role = account.role; // <-- AFTER
+            // const refreshTokens = await account.getRefreshTokens();
+            // req.user.ownsToken = (token: any) => !!refreshTokens.find((x: any) => x.token === token);
+            // next();
 
             if (!account || (roles.length && !roles.includes(account.role))) {
                 return res.status(401).json({ message: 'Unauthorized' });
             }
 
-            req.user.role = account.role;
             const refreshTokens = await account.getRefreshTokens();
-            req.user.ownsToken = (token: any) => !!refreshTokens.find((x: any) => x.token === token);
+
+
+            req.user = {
+                ...req.auth,
+                role: account.role,
+                ownsToken: (token: any) => !!refreshTokens.find((x: any) => x.token === token)
+            };
+
             next();
         }
     ];
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { initialize } from "./_helpers/db"
 import userController from "./users/users.controller"
 import swaggerRouter from "./_helpers/swagger"
 import accountsController from "./accounts/account.controller"
+import cookieParser from "cookie-parser"
 
 const app: Application = express()
 
@@ -12,6 +13,7 @@ const app: Application = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(cookieParser())
 
 app.use("/accounts", accountsController)
 app.use("/api-docs", swaggerRouter)
