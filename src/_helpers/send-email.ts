76eaf6719e7485139729c53bbaba72e6563c2233
Danley Yap, Galan <galandanley701254@gmail.com>
2026-05-05 13:47:48 +0800

    Switch to gmail smtp instead of ethereal for testing because ethereal does not work:<>

--- src/_helpers/send-email.ts
+++ src/_helpers/send-email.ts
@@ -2,6 +2,11 @@ import nodemailer from "nodemailer"
 import config from "../../config.prod.json"
 
 export default async function sendEmail({ to, subject, html, from = config.emailform }: any) {
-    const transporter = nodemailer.createTransport(config.smtOptions)
+    const transporter = nodemailer.createTransport({
+        ...config.smtOptions,
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
+    })
     await transporter.sendMail({ from, to, subject, html })
 }
